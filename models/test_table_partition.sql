05357317faa30f3ec521020c695ac3d5a6cc2b62
Nick <myfjdthink@163.com>
2022-05-06 11:03:26 +0800

    feat: more test

--- models/test_table_partition.sql
+++ models/test_table_partition.sql
@@ -1,8 +1,19 @@
 {{ config(
     materialized='table',
-    partition_by = ['calling_code']
-)}}
+    partition_by = ['calling_code'],
+    partition_type = "RANGE",
+    partition_values = [
+        ('p1', 'LESS THAN ("50")'),
+        ('p2', 'LESS THAN ("500")')
+    ],
+    distributed_by = ['country_code'],
+    buckets = 2,
+    properties = {
+        "replication_num" : 2
+    }
+)
+}}
 
-select
-*
-from {{ref("country_codes")}} limit 100
+select *
+from {{ref("country_codes")}}
+limit 100
